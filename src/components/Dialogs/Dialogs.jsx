1e427439901515793b535772aea4be08084c744e
Ars <arsenkaribaev98@gmail.com>
2022-06-22 15:34:47 +0300

    21-th lession

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,38 @@ import s from './Dialogs.module.css';
 
 const Dialogs = () => {
     return(
-        <div>
-            Hi
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active}>
+                    Malyshka
+                </div>
+                <div className={s.dialog}>
+                    Senpai
+                </div>
+                <div className={s.dialog}>
+                    Artur
+                </div>
+                <div className={s.dialog}>
+                    Amina
+                </div>
+                <div className={s.dialog}>
+                    Sabira
+                </div>
+                <div className={s.dialog}>
+                    Den
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.massage}>
+                    Кисконайым
+                </div>
+                <div className={s.massage}>
+                    Еркеее
+                </div>
+                <div className={s.massage}>
+                    Сянм сянм сянм
+                </div>
+            </div>
         </div>
     )
 }
