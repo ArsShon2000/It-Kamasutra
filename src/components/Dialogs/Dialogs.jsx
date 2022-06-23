477e0dd0c46e49c03027c23af9b3cba4998e2431
Ars <arsenkaribaev98@gmail.com>
2022-06-24 02:22:09 +0300

    26-th lession Инкапсуляция диалогов и сообщений

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,19 +1,20 @@
 import React, { component } from 'react';
-import { NavLink } from 'react-router-dom';
+import DialogItem from './DialogItem/DialogItem';
 import s from './Dialogs.module.css';
+import Message from './Message/Message';
 
-const DialogItem = (props) => {
-    let path = "/dialogs/" + props.id;
-    return <div className={s.dialog + ' ' + s.active}>
-        <NavLink to={path}>{props.name}</NavLink>
-    </div>
-}
+// const DialogItem = (props) => {
+//     let path = "/dialogs/" + props.id;
+//     return <div className={s.dialog + ' ' + s.active}>
+//         <NavLink to={path}>{props.name}</NavLink>
+//     </div>
+// }
 
-const Message = (props) => {
-    return <div className={s.dialog}>
-        {props.message}
-    </div>
-}
+// const Message = (props) => {
+//     return <div className={s.dialog}>
+//         {props.message}
+//     </div>
+// }
 
 const Dialogs = (props) => {
 
