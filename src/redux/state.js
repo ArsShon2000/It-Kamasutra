4e198931cda62b879091c9e1cc3ef60165c5b472
Ars <arsenkaribaev98@gmail.com>
2022-06-26 17:31:25 +0300

    37-th lession (OOP)

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,75 +1,74 @@
 import React from "react";
 
-let rerenderEntireTree = () => {
-  console.log('State is changed')
-}
+let store = {
+  _state : {
 
-let state = {
+    profilePage: {
+      posts: [
+        { id: 1, message: 'Hi, my name is Ars', likesCount: 7 },
+        { id: 2, message: "It's my first post", likesCount: 6 },
+        { id: 3, message: 'I learn React 3-th day', likesCount: 47 },
+        { id: 4, message: 'Я будущий миллионер', likesCount: 87237 },
+      ],
 
-  profilePage: {
-    posts: [
-      { id: 1, message: 'Hi, my name is Ars', likesCount: 7 },
-      { id: 2, message: "It's my first post", likesCount: 6 },
-      { id: 3, message: 'I learn React 3-th day', likesCount: 47 },
-      { id: 4, message: 'Я будущий миллионер', likesCount: 87237 },
-    ],
+      newPostText: "More money",
 
-    newPostText: "More money",
+    },
 
+    dialogsPage: {
+      dialogs: [
+        { id: 1, name: 'Malyshka', avatar: 'https://i.pinimg.com/236x/96/57/38/9657382007db8eb906fca1a9214f53df.jpg' },
+        { id: 2, name: 'Senpai', avatar: 'https://vgtimes.ru/uploads/posts/2021-06/1623445957_tr8dqutdpwk.jpg' },
+        { id: 3, name: 'Artur', avatar: 'https://pbs.twimg.com/media/EsBDBcnXcAEgDBX.jpg' },
+        { id: 4, name: 'Amina', avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgOusP5YliNsZbYwI4WkynTqKHUVyQFFWHQQ&usqp=CAU' },
+        { id: 5, name: 'Sabira', avatar: 'https://i.pinimg.com/564x/33/74/fd/3374fdc57e601e40d61d2001dd861454.jpg' },
+        { id: 6, name: 'Den', avatar: 'https://yt3.ggpht.com/ByOVkU5hN6Eal5Cs009IihRYm8YyIq5MAT_nc93WU43LXY4RKqO3pVT41OkWF0FwgH-l2iduEg=s900-c-k-c0x00ffffff-no-rj' },
+      ],
+
+      messages: [
+        { id: 1, message: 'Кисконайым' },
+        { id: 2, message: 'Еркеее' },
+        { id: 3, message: 'Сянм сянм сянм' },
+        { id: 4, message: 'Сянм сянм сянм' },
+        { id: 5, message: 'Сянм сянм сянм' },
+        { id: 6, message: 'Сянм сянм сянм' },
+      ]
+    },
+
+    sidebar: {
+      avatars: [
+        { avatar: 'https://vgtimes.ru/uploads/posts/2021-06/1623445957_tr8dqutdpwk.jpg' },
+        { avatar: 'https://vgtimes.ru/uploads/posts/2021-06/1623445957_tr8dqutdpwk.jpg' },
+        { avatar: 'https://vgtimes.ru/uploads/posts/2021-06/1623445957_tr8dqutdpwk.jpg' },
+      ]
+
+    },
   },
-
-  dialogsPage: {
-    dialogs: [
-      { id: 1, name: 'Malyshka', avatar: 'https://i.pinimg.com/236x/96/57/38/9657382007db8eb906fca1a9214f53df.jpg' },
-      { id: 2, name: 'Senpai', avatar: 'https://vgtimes.ru/uploads/posts/2021-06/1623445957_tr8dqutdpwk.jpg' },
-      { id: 3, name: 'Artur', avatar: 'https://pbs.twimg.com/media/EsBDBcnXcAEgDBX.jpg' },
-      { id: 4, name: 'Amina', avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgOusP5YliNsZbYwI4WkynTqKHUVyQFFWHQQ&usqp=CAU' },
-      { id: 5, name: 'Sabira', avatar: 'https://i.pinimg.com/564x/33/74/fd/3374fdc57e601e40d61d2001dd861454.jpg' },
-      { id: 6, name: 'Den', avatar: 'https://yt3.ggpht.com/ByOVkU5hN6Eal5Cs009IihRYm8YyIq5MAT_nc93WU43LXY4RKqO3pVT41OkWF0FwgH-l2iduEg=s900-c-k-c0x00ffffff-no-rj' },
-    ],
-
-    messages: [
-      { id: 1, message: 'Кисконайым' },
-      { id: 2, message: 'Еркеее' },
-      { id: 3, message: 'Сянм сянм сянм' },
-      { id: 4, message: 'Сянм сянм сянм' },
-      { id: 5, message: 'Сянм сянм сянм' },
-      { id: 6, message: 'Сянм сянм сянм' },
-    ]
+  getState() {
+      return this._state
   },
-
-  sidebar: {
-    avatars: [
-      { avatar: 'https://vgtimes.ru/uploads/posts/2021-06/1623445957_tr8dqutdpwk.jpg' },
-      { avatar: 'https://vgtimes.ru/uploads/posts/2021-06/1623445957_tr8dqutdpwk.jpg' },
-      { avatar: 'https://vgtimes.ru/uploads/posts/2021-06/1623445957_tr8dqutdpwk.jpg' },
-    ]
-
+  _callSubscriber () {
+    console.log('State is changed')
+  },
+  addPost ()  {
+    let newPost = {
+      id: 5,
+      message: this._state.profilePage.newPostText,
+      likesCount: 0,
+    };
+  
+    this._state.profilePage.posts.push(newPost);
+    this._state.profilePage.newPostText = ''
+    this._callSubscriber(this._state);
+  },
+  updateNewPostText (newText) {
+    this._state.profilePage.newPostText = newText
+    this._callSubscriber(this._state);
+  },
+  subscribe (observer) {
+    this._callSubscriber = observer;
   },
 }
 
-window.state = state
-
-export const addPost = () => {
-  let newPost = {
-    id: 5,
-    message: state.profilePage.newPostText,
-    likesCount: 0
-  };
-
-  state.profilePage.posts.push(newPost);
-  state.profilePage.newPostText = ''
-  rerenderEntireTree(state);
-}
-
-export const updateNewPostText = (newText) => {
-  state.profilePage.newPostText = newText
-  rerenderEntireTree(state);
-}
-
-export const subscribe = (observer) => {
-  rerenderEntireTree = observer;
-}
-
-//we learning OOP
-export default state;
+export default store;
+window.state = store
