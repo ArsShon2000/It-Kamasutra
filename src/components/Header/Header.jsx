82dbbe0662eef14e2be2619fac4042389ae63549
Ars <arsenkaribaev98@gmail.com>
2022-07-07 23:56:40 +0300

    after 47-th lesson I tryed change is dialogReducer

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import s from './Header.module.css';
 
 const Header = () => {
     return <header className={s.header}>
-        <img src='https://sun9-47.userapi.com/impg/3uS6CDwUbpMKhngaasrMOWpoAbZ6PaPS4BMuVg/vyTAYiFI6LQ.jpg?size=1391x1440&quality=96&sign=849d1c5d4beb9b4870b2d2acc78cef03&type=album' />
+        {/* <img src='https://sun9-47.userapi.com/impg/3uS6CDwUbpMKhngaasrMOWpoAbZ6PaPS4BMuVg/vyTAYiFI6LQ.jpg?size=1391x1440&quality=96&sign=849d1c5d4beb9b4870b2d2acc78cef03&type=album' /> */}
     </header>
 }
 
--- src/redux/dialogsReducer.js
+++ src/redux/dialogsReducer.js
@@ -2,41 +2,47 @@ const UPDATE_NEW_MESSAGE_BODY = 'UPDATE_NEW_MESSAGE_BODY'
 const SEND_MESSAGE = 'SEND_MESSAGE'
 
 let initialState = {
-    dialogs: [
-      { id: 1, name: 'Malyshka', avatar: 'https://i.pinimg.com/236x/96/57/38/9657382007db8eb906fca1a9214f53df.jpg' },
-      { id: 2, name: 'Senpai', avatar: 'https://vgtimes.ru/uploads/posts/2021-06/1623445957_tr8dqutdpwk.jpg' },
-      { id: 3, name: 'Artur', avatar: 'https://pbs.twimg.com/media/EsBDBcnXcAEgDBX.jpg' },
-      { id: 4, name: 'Amina', avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgOusP5YliNsZbYwI4WkynTqKHUVyQFFWHQQ&usqp=CAU' },
-      { id: 5, name: 'Sabira', avatar: 'https://i.pinimg.com/564x/33/74/fd/3374fdc57e601e40d61d2001dd861454.jpg' },
-      { id: 6, name: 'Den', avatar: 'https://yt3.ggpht.com/ByOVkU5hN6Eal5Cs009IihRYm8YyIq5MAT_nc93WU43LXY4RKqO3pVT41OkWF0FwgH-l2iduEg=s900-c-k-c0x00ffffff-no-rj' },
-    ],
+  dialogs: [
+    { id: 1, name: 'Malyshka', avatar: 'https://i.pinimg.com/236x/96/57/38/9657382007db8eb906fca1a9214f53df.jpg' },
+    { id: 2, name: 'Senpai', avatar: 'https://vgtimes.ru/uploads/posts/2021-06/1623445957_tr8dqutdpwk.jpg' },
+    { id: 3, name: 'Artur', avatar: 'https://pbs.twimg.com/media/EsBDBcnXcAEgDBX.jpg' },
+    { id: 4, name: 'Raushanka', avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgOusP5YliNsZbYwI4WkynTqKHUVyQFFWHQQ&usqp=CAU' },
+    { id: 5, name: 'Dinka', avatar: 'https://i.pinimg.com/564x/33/74/fd/3374fdc57e601e40d61d2001dd861454.jpg' },
+    { id: 6, name: 'Den', avatar: 'https://i.pinimg.com/550x/19/b5/ee/19b5eecf61ecfdf1b69516ad3a402aae.jpg' },
+  ],
 
-    messages: [
-      { id: 1, message: 'Кисконайым' },
-      { id: 2, message: 'Еркеее' },
-      { id: 3, message: 'Сянм сянм сянм' },
-      { id: 4, message: 'Сянм сянм сянм' },
-      { id: 5, message: 'Сянм сянм сянм' },
-      { id: 6, message: 'Сянм сянм сянм' },
-    ],
+  messages: [
+    { id: 1, message: 'Кисконайым' },
+    { id: 2, message: 'Еркеее' },
+    { id: 3, message: 'Сянм сянм сянм' },
+    { id: 4, message: 'Сянм сянм сянм' },
+    { id: 5, message: 'Сянм сянм сянм' },
+    { id: 6, message: 'Сянм сянм сянм' },
+  ],
 
-    newMessageBody: [
-    ]
-  }
+  newMessageBody: ""
+}
 
 const dialogsReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case UPDATE_NEW_MESSAGE_BODY:
-            state.newMessageBody = action.body
-            return state
-        case SEND_MESSAGE:
-            let body = state.newMessageBody
-            state.newMessageBody = ' '
-            state.messages.push({ id: 7, message: body })
-            return state
-        default:
-            return state
+  let stateCopy = { 
+    ...state,
+    // messages: [...state.messages],
+   }
+
+  switch (action.type) {
+    case UPDATE_NEW_MESSAGE_BODY: {
+      stateCopy.newMessageBody = action.body
+      return stateCopy
     }
+    case SEND_MESSAGE: {
+      let body = state.newMessageBody
+      stateCopy.newMessageBody = ''
+      stateCopy.messages.push({ id: 7, message: body })
+      return stateCopy
+    }
+    default:
+      return state
+  }
 }
 
 export const sendMessageCreator = () => ({ type: SEND_MESSAGE })
