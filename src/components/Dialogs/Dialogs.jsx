ec4c6cbd9e2fea8948d6627d277d0d4cd965691b
Ars <arsenkaribaev98@gmail.com>
2022-07-08 00:23:37 +0300

    48-th lesson (deep copy and shallow copy DialogPage (to be continue))

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -16,8 +16,8 @@ const Dialogs = (props) => {
         let body = e.target.value
         props.updateNewMessageBody(body)}
 
-    let dialogElements = state.dialogs.map(d => <DialogItem name={d.name} id={d.id} avatar={d.avatar} />);
-    let messagesElements = state.messages.map(m => <Message message={m.message} />);
+    let dialogElements = state.dialogs.map(d => <DialogItem name={d.name} id={d.id} key = {d.id} avatar={d.avatar} />);
+    let messagesElements = state.messages.map(m => <Message message={m.message} key = {m.id}  />);
     let newMessageBody = state.newMessageBody
 
     return (
