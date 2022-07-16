b9ee4f80c6f9a310546b3ed7074a56f4412379f6
Ars <arsenkaribaev98@gmail.com>
2022-07-17 00:40:48 +0300

    52-th lession (Side effect)

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,22 +4,26 @@ import styles from './Users.module.css'
 import userPhoto from '../../assets/images/user.jpg'
 
 let Users = (props) => {
-    if (props.users.length === 0) {
-        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-            props.setUsers(response.data.items)
-    });
-        // props.setUsers( [
-        //     { id: 1, followed: true, fullName: 'Malyshka', status: 'I am Ars is girlfriend', location: { city: 'Moscow', country: 'Russia' }, photoUrl: 'https://i.pinimg.com/236x/96/57/38/9657382007db8eb906fca1a9214f53df.jpg' },
-        //     { id: 2, followed: true, fullName: 'Senpai', status: 'I am Ars is girlfriend', location: { city: 'Moscow', country: 'Russia' }, photoUrl: 'https://vgtimes.ru/uploads/posts/2021-06/1623445957_tr8dqutdpwk.jpg' },
-        //     { id: 3, followed: true, fullName: 'Artur', status: 'I am Ars is girlfriend', location: { city: 'Moscow', country: 'Russia' }, photoUrl: 'https://pbs.twimg.com/media/EsBDBcnXcAEgDBX.jpg' },
-        //     { id: 4, followed: false, fullName: 'Raushanka', status: 'I am Ars is girlfriend', location: { city: 'Moscow', country: 'Russia' }, photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgOusP5YliNsZbYwI4WkynTqKHUVyQFFWHQQ&usqp=CAU' },
-        //     { id: 5, followed: false, fullName: 'Dinka', status: 'I am Ars is girlfriend', location: { city: 'Moscow', country: 'Russia' }, photoUrl: 'https://i.pinimg.com/564x/33/74/fd/3374fdc57e601e40d61d2001dd861454.jpg' },
-        //     { id: 6, followed: true, fullName: 'Den', status: 'I am Ars is girlfriend', location: { city: 'Moscow', country: 'Russia' }, photoUrl: 'https://i.pinimg.com/550x/19/b5/ee/19b5eecf61ecfdf1b69516ad3a402aae.jpg' }
-        // ]   )
+
+    let getUsers = () => {
+        if (props.users.length === 0) {
+            axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+                props.setUsers(response.data.items)
+            });
+            // props.setUsers( [
+            //     { id: 1, followed: true, fullName: 'Malyshka', status: 'I am Ars is girlfriend', location: { city: 'Moscow', country: 'Russia' }, photoUrl: 'https://i.pinimg.com/236x/96/57/38/9657382007db8eb906fca1a9214f53df.jpg' },
+            //     { id: 2, followed: true, fullName: 'Senpai', status: 'I am Ars is girlfriend', location: { city: 'Moscow', country: 'Russia' }, photoUrl: 'https://vgtimes.ru/uploads/posts/2021-06/1623445957_tr8dqutdpwk.jpg' },
+            //     { id: 3, followed: true, fullName: 'Artur', status: 'I am Ars is girlfriend', location: { city: 'Moscow', country: 'Russia' }, photoUrl: 'https://pbs.twimg.com/media/EsBDBcnXcAEgDBX.jpg' },
+            //     { id: 4, followed: false, fullName: 'Raushanka', status: 'I am Ars is girlfriend', location: { city: 'Moscow', country: 'Russia' }, photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgOusP5YliNsZbYwI4WkynTqKHUVyQFFWHQQ&usqp=CAU' },
+            //     { id: 5, followed: false, fullName: 'Dinka', status: 'I am Ars is girlfriend', location: { city: 'Moscow', country: 'Russia' }, photoUrl: 'https://i.pinimg.com/564x/33/74/fd/3374fdc57e601e40d61d2001dd861454.jpg' },
+            //     { id: 6, followed: true, fullName: 'Den', status: 'I am Ars is girlfriend', location: { city: 'Moscow', country: 'Russia' }, photoUrl: 'https://i.pinimg.com/550x/19/b5/ee/19b5eecf61ecfdf1b69516ad3a402aae.jpg' }
+            // ]   )
+        }
     }
 
     return (
         <div>
+            <button onClick={getUsers}>Get Users</button>
             {
                 props.users.map(u => <div key={u.id} >
                     <span>
