f4a166fad435235c3507ff05fd3d2813a4dfa1ee
Ars <arsenkaribaev98@gmail.com>
2022-07-23 17:01:53 +0300

    Can't to do it

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -3,25 +3,36 @@ import Profile from './Profile';
 import axios from 'axios'
 import { setUserProfile } from '../../redux/profileReducer';
 import {connect} from 'react-redux'
-import { withRouter } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 
+
+const WithRouterComponent = (props) => {
+    const param = useParams()
+    debugger
+    return(
+        <ProfileContainer {...props} 
+        userId = {param.userId ? param.userId: 2}
+        />
+    )
+}
 
 class ProfileContainer  extends React.Component {
 
     
 
     componentDidMount() {
-        let userId = this.props.match.params.userId
-        if(!userId) {
-            userId = 2
-        }
-        axios.get(`https://social-network.samuraijs.com/api/1.0/profile/` + userId)
+        // let userId = this.props.match.params.userId
+        // if(!userId) {
+        //     userId = 2
+        // }
+        axios.get(`https://social-network.samuraijs.com/api/1.0/profile/${this.props.userId}`)
         .then(response => {
-            
+            debugger
             this.props.setUserProfile(response.data)
         });
     }
 
+    
     
 
     render(){
@@ -31,10 +42,25 @@ class ProfileContainer  extends React.Component {
     }
 }
 
+
+
 let mapStateToProps = (state) => ({
     profile: state.profilePage.profile
 })
 
-let withUrlDataContainerComponent = withRouter(ProfileContainer)
+// export function withRouter(Children){
+//     return(props) => {
+//         debugger
+//         const match = {params: useParams()}
+//         return <Children {...props} match = {match} />
+//     }
+// }
 
-export default connect(mapStateToProps, {setUserProfile}) (withUrlDataContainerComponent);
+
+// let WithUrlDataContainerComponent = withRouter(ProfileContainer)
+
+
+
+
+
+export default connect(mapStateToProps, {setUserProfile})(WithRouterComponent);
