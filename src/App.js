6858fd36a51f2ac26084b6c4afed2e9441639f95
Ars <arsenkaribaev98@gmail.com>
2022-07-24 11:26:40 +0300

    61-st lesson (login auth/me cookie )

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 // import logo from './logo.svg';
 import './App.css';
-import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import News from './components/News/News'
 import Music from './components/Music/Music'
@@ -10,13 +9,14 @@ import Settings from './components/Settings/Settings'
 import DialogsContainer from './components/Dialogs/DialogsContainer';
 import UsersContainer from './components/Users/UsersContainer';
 import ProfileContainer from './components/Profile/ProfileContainer';
+import HeaderContainer from './components/Header/HeaderContainer';
 
 
 const App = () => {
   return (
     <BrowserRouter>
       <div className="App-wrapper">
-        <Header />
+        <HeaderContainer />
         <Navbar />
         <div className='App-wrapper-content'>
           <Route path='/dialogs'
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,14 @@
 import React, { component } from 'react';
+import { NavLink } from 'react-router-dom';
 import s from './Header.module.css';
 
-const Header = () => {
+const Header = (props) => {
     return <header className={s.header}>
-        {/* <img src='https://sun9-47.userapi.com/impg/3uS6CDwUbpMKhngaasrMOWpoAbZ6PaPS4BMuVg/vyTAYiFI6LQ.jpg?size=1391x1440&quality=96&sign=849d1c5d4beb9b4870b2d2acc78cef03&type=album' /> */}
+        <img src='https://sun9-47.userapi.com/impg/3uS6CDwUbpMKhngaasrMOWpoAbZ6PaPS4BMuVg/vyTAYiFI6LQ.jpg?size=1391x1440&quality=96&sign=849d1c5d4beb9b4870b2d2acc78cef03&type=album' />
+        <div className={s.loginBlock}>
+            {props.isAuth ? props.login
+            :<NavLink to={'/login'}>login</NavLink>}
+        </div>
     </header>
 }
 
