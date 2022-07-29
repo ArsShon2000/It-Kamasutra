5ee91aeb937c6864a2c5ce8e416d94b524da57bb
Ars <arsenkaribaev98@gmail.com>
2022-07-29 08:48:25 +0300

    I restored old file

--- src/components/Header/HeaderContainer.jsx
+++ src/components/Header/HeaderContainer.jsx
@@ -1,13 +1,21 @@
 import axios from 'axios';
 import React from 'react';
 import { connect } from 'react-redux';
-import { setAuthUserData, logInToTheSide } from '../../redux/authReducer';
+import { setAuthUserData } from '../../redux/authReducer';
 import Header from './Header';
 
 class HeaderContainer extends React.Component {
 
     componentDidMount(){
-        this.props.logInToTheSide()
+        // this.props.logInToTheSide()
+        
+    axios.get('https://social-network.samuraijs.com/api/1.0/auth/me', {withCredentials: true})
+    .then(response => {
+        if (response.data.resultCode ===0 ){
+            let {id, email, login} = response.data.data
+            this.props.setAuthUserData(id, email, login)
+        }
+    });
     }
 
     render() {
@@ -20,4 +28,4 @@ const mapStateToProps = (state) => ({
     login: state.auth.login,
 })
 
-export default connect (mapStateToProps, {setAuthUserData, logInToTheSide}) (HeaderContainer)
+export default connect (mapStateToProps, {setAuthUserData}) (HeaderContainer)
